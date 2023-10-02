8c9623f3a197cc8647b525b34f147f92a34ac6aa
Tobex <89360416+Tobexint@users.noreply.github.com>
2023-10-02 14:36:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ import subprocess
 import whisper
 model = whisper.load_model("base")
 
-#path to recorded video on local device
-video_in = 'video.mp4'
+def transcribe():
+  
+  #path to recorded video on local device
+  video_in = 'video.mp4'
 
-#path to mp3 for video conversion
-audio_out = 'audio.mp3'
+  #path to mp3 for video conversion
+  audio_out = 'audio.mp3'
 
-ffmpeg_cmd = f" ffmpeg -i {video_in} -vn -c:a libmp3lame -b:a 192k {audio_out}"
+  ffmpeg_cmd = f" ffmpeg -i {video_in} -vn -c:a libmp3lame -b:a 192k {audio_out}"
 
-subprocess.run(["ffmpeg", "-i", video_in, "-vn", "-c:a", "libmp3lame", "-b:a", "192k", audio_out])
+  subprocess.run(["ffmpeg", "-i", video_in, "-vn", "-c:a", "libmp3lame", "-b:a", "192k", audio_out])
 
-result = model.transcribe(audio_out)
-print(result["text"])
+  result = model.transcribe(audio_out)
+  print(result["text"])
