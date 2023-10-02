2429daca792689e28ee64c947c26125055ea5a0f
tobexint <tobexint@gmail.com>
2023-10-02 11:36:56 +0000

    video transcription

---
+++ main.py
@@ -0,0 +1,20 @@
+#pip install openai-whisper
+#pip install ffmpeg
+#brew install ffmpeg
+
+import subprocess
+import whisper
+model = whisper.load_model("base")
+
+#path to recorded video on local device
+video_in = 'video.mp4'
+
+#path to mp3 for video conversion
+audio_out = 'audio.mp3'
+
+ffmpeg_cmd = f" ffmpeg -i {video_in} -vn -c:a libmp3lame -b:a 192k {audio_out}"
+
+subprocess.run(["ffmpeg", "-i", video_in, "-vn", "-c:a", "libmp3lame", "-b:a", "192k", audio_out])
+
+result = model.transcribe(audio_out)
+print(result["text"])
