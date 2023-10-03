e1e4be7311ac77e5526e0b666c2b9ccdc4de339f
Tobex <89360416+Tobexint@users.noreply.github.com>
2023-10-03 01:49:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-#import os
+from flask_cors import CORS
+import os
 
-#basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///videos.db'
 db = SQLAlchemy(app)
+CORS(app)
 
 #Video Model
 class Video(db.Model):
